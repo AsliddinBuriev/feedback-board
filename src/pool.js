ebbc4f217560b68f5236803dc9eda14d56c8f392
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-04-28 21:39:25 +0900

    delete git history

---
+++ src/pool.js
@@ -0,0 +1,16 @@
+import pg from 'pg';
+
+class Pool {
+	_pool = null;
+	connect = (opt) => {
+		this._pool = new pg.Pool(opt);
+		return this._pool.query('SELECT 1 + 1;');
+	};
+	close = () => {
+		this._pool.end();
+	};
+	query = (sql, params) => {
+		return this._pool.query(sql);
+	};
+}
+export default new Pool();
