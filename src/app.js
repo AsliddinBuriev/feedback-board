7e32d3060f09e120674899daffd35571b9a480e7
Asliddin Buriev <68139371+AsliddinBuriev@users.noreply.github.com>
2022-04-28 18:39:00 +0900

    Revert "user route set up"

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import userRouter from './routes/user.routes.js';
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api/v1/users', userRouter);
 
 export default app;
